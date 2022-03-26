f08ec52ff2a566bd2105663cb9aa2ff07dbfdb91
PodFolio <66847230+PodFolio@users.noreply.github.com>
2022-03-26 05:35:32 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ConvCEMRims
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(1, 1);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(257, 407);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(258, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Convert selected";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(258, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Convert\r\nAll";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button14
+            // 
+            this.button14.Image = global::ConvCEMRims.Properties.Resources.iconConnecting;
+            this.button14.Location = new System.Drawing.Point(258, 92);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(92, 24);
+            this.button14.TabIndex = 46;
+            this.toolTip1.SetToolTip(this.button14, "Refresh");
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::ConvCEMRims.Properties.Resources.PFsq_sml2;
+            this.pictureBox1.Location = new System.Drawing.Point(258, 325);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(92, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.toolTip1.SetToolTip(this.pictureBox1, "PodFolio.eu");
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 409);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Convert CEM Rims To SPK";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace ConvCEMRims
+{
+    public partial class Form1 : Form
+    {
+        string CurrentDirectory = (Environment.CurrentDirectory);
+        int indx_start = 1;
+        int indx_shit = 0;
+        int indx_revshit = 4;
+        int indx_shit2 = 0;
+        int indx_revshit2 = 4;
+
+
+        int useles;
+        int shit;
+        int at;
+        int start;
+        int offile;
+        byte[] rimshit = new byte[200000];
+        byte filever = 0xFB;
+        int useles2;
+        int shit2;
+        int at2;
+        int start2;
+        int offile2;
+        byte[] rimshit2 = new byte[200000];
+
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            cem_List();
+        }
+        private void cem_List()
+        {
+            if (indx_start == 1)
+            {
+                listBox1.Items.Clear();
+                string[] files = Directory.GetFiles(CurrentDirectory, "*.cem");
+                foreach (string file in files)
+                {
+                    listBox1.Items.Add(Path.GetFileNameWithoutExtension(file));
+                }
+                indx_start = 2;
+
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string file = CurrentDirectory + "\\" + listBox1.SelectedItem.ToString() + ".cem";
+            string name = CurrentDirectory + "\\SPK\\" + listBox1.SelectedItem.ToString() + ".spk";
+            try
+            {
+                FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read);
+
+
+                BinaryReader rd = new BinaryReader(fs);
+                
+                    
+                    useles = rd.ReadInt32();
+                    shit = rd.ReadInt32();
+                    at = rd.ReadInt32();
+                    start = rd.ReadInt32();
+                    offile = rd.ReadInt32();
+                    while (rd.BaseStream.Position < rd.BaseStream.Length)
+                    {
+                        rimshit[rd.BaseStream.Position] = rd.ReadByte();
+                        indx_shit += 1;
+                    }
+                    rd.Close();
+
+                
+                
+                fs.Close();
+                
+
+            }
+            catch (IOException)
+            { }
+
+            string srs = "LFSSPK\0";
+            byte[] boshe = Encoding.ASCII.GetBytes(srs);
+            BinaryWriter file2 = new BinaryWriter(File.Open(name, FileMode.Create));
+            file2.Write(boshe);
+            file2.Write(filever);
+            while (indx_revshit < indx_shit+20)
+            {
+                file2.Write(rimshit[indx_revshit]);
+                indx_revshit += 1;
+            }
+            file2.Close();
+            indx_shit = 0;
+            indx_revshit = 4;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string[] filesZ = Directory.GetFiles(CurrentDirectory, "*.cem");
+            foreach (string file_ in filesZ)
+            {
+
+                string fileZ = CurrentDirectory + "\\" + Path.GetFileNameWithoutExtension(file_).ToString() + ".cem";
+                string nameZ = CurrentDirectory + "\\SPK\\" + Path.GetFileNameWithoutExtension(file_).ToString() + ".spk";
+                try
+                {
+                    FileStream fsZ = new FileStream(fileZ, FileMode.Open, FileAccess.Read);
+
+                    BinaryReader rdZ = new BinaryReader(fsZ);
+
+
+                    useles2 = rdZ.ReadInt32();
+                    shit2 = rdZ.ReadInt32();
+                    at2 = rdZ.ReadInt32();
+                    start2 = rdZ.ReadInt32();
+                    offile2 = rdZ.ReadInt32();
+                    while (rdZ.BaseStream.Position < rdZ.BaseStream.Length)
+                    {
+                        rimshit2[rdZ.BaseStream.Position] = rdZ.ReadByte();
+                        indx_shit2 += 1;
+                    }
+                    rdZ.Close();
+
+
+
+                    fsZ.Close();
+
+
+                }
+                catch (IOException)
+                { }
+
+                string srs = "LFSSPK\0";
+                byte[] boshe = Encoding.ASCII.GetBytes(srs);
+                BinaryWriter file2Z = new BinaryWriter(File.Open(nameZ, FileMode.Create));
+                file2Z.Write(boshe);
+                file2Z.Write(filever);
+                while (indx_revshit2 < indx_shit2 + 20)
+                {
+                    file2Z.Write(rimshit2[indx_revshit2]);
+                    indx_revshit2 += 1;
+                }
+                file2Z.Close();
+                indx_shit2 = 0;
+                indx_revshit2 = 4;
+
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://podfolio.eu/lfs-mods");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            indx_start = 1;
+            cem_List();
+        }
+    }
+}
